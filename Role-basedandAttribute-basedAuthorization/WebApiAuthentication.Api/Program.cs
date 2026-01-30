132c4d534aa341d55f32824aceaf46bf4cf724ac
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-01-30 21:12:09 +0330

    Role-basedAuthorization

---
+++ Role-basedandAttribute-basedAuthorization/WebApiAuthentication.Api/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer();  
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
